eed379f007dbd5c66639c1369e6615e587588ab7
itsay-mane <itsay2003@gmail.com>
2025-11-13 12:37:33 +0100

    graphe

---
+++ TestGrapheSuccesseurs.java
@@ -0,0 +1,18 @@
+package src;
+
+public class TestGrapheSuccesseurs {
+    public static void main(String[] args) {
+        GrapheSuccesseurs gs = new GrapheSuccesseurs(
+                new Sommet("a"),
+                new Sommet("b"),
+                new Sommet("c")
+        );
+        gs.ajouteArc("a", "b");
+        System.out.println(gs);
+        System.out.println(gs.existeArc("a", "b"));
+        System.out.println(gs.existeArc("b", "a"));
+        System.out.println(gs.existeChemin("a", "b"));
+        gs.ajouteArc("b", "c");
+        System.out.println(gs.existeChemin("a", "c"));
+    }
+}
